dfd6acddfdaa8cbe970c7672d8e06af282d07bc6
v.svistunov <v.svistunov@corp.nekki.ru>
2024-07-16 21:12:17 +0300

    механика поиска звука рекордером

---
+++ foundsound_new/Assets/HealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public float oscillationSpeed = 1f; // скорость колебаний
+    private float originalHealth; // изначальное значение health
+
+    private void Start()
+    {
+        originalHealth = slider.value;
+       // StartCoroutine(OscillateHealth());
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        originalHealth = health; // обновляем изначальное значение
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health + Random.Range(-0.02f, 0.02f); 
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+  
+}
---
+++ foundsound_new/Assets/Scripts/SignalController.cs
@@ -0,0 +1,153 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using TMPro;
+
+public class SignalController : MonoBehaviour
+{
+    public Transform playerCamera;
+    public List<Transform> emitersToAdd;
+    public List<Transform> emiters;// изменение массива на список
+    public float signalForce = 1f;
+    public TextMeshProUGUI signalText;
+    public float maxSignalDistance = 10f;
+    public float signalImpactValue = 0.5f;
+    public float distanceImpactValue = 0.5f;
+    
+    [Space(10)] 
+    public HealthBar signalBar;
+    
+    private float maxSignal = 0f;
+    public float delayTime = 0.3f;
+    private Coroutine addingEmiter;
+    
+    void Update()
+    {
+        maxSignal = 0f;
+        
+        HandleEmiters();
+        
+        signalText.SetText("Signal: " + maxSignal.ToString("F2"));
+        signalBar.SetHealth(maxSignal);
+    }
+    
+    private void HandleEmiters()
+    {
+        if(emiters.Count == 0)
+            return;
+        
+        foreach (Transform locator in emiters)
+        {
+            Vector3 directionToLocator = locator.position - playerCamera.position;
+            float distanceToLocator = directionToLocator.magnitude;
+            directionToLocator.Normalize();
+            Vector3 cameraForward = playerCamera.forward;
+            float angle = Vector3.Angle(cameraForward, directionToLocator) / 90f;
+            
+            float signalImpact = Mathf.Clamp01(1 - angle * signalForce);
+            signalImpact *= signalImpactValue;
+            
+            // Вычисляем влияние расстояния
+            float distanceFactor = 1f - Mathf.Clamp01(distanceToLocator / maxSignalDistance);
+            float distanceImpact = Mathf.Clamp01(distanceImpactValue * distanceFactor);
+            
+            float signal = distanceImpact + signalImpact;
+            
+
+            if (signal > maxSignal)
+            {
+                maxSignal = signal;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("emiter"))
+        {
+            // Добавляем эмиттер в список emitersToAdd
+            if (!emitersToAdd.Contains(other.transform))
+            {
+                emitersToAdd.Add(other.transform);
+                addingEmiter = StartCoroutine(AddEmiterAfterDelay(other.transform));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("emiter"))
+        {
+            if (emitersToAdd.Contains(other.transform))
+            {
+                StopCoroutine(addingEmiter);
+                emitersToAdd.Remove(other.transform);
+               
+                
+                Debug.Log("Unregistered add: " + other.name);
+            }
+            if (emiters.Contains(other.transform))
+            {
+                emiters.Remove(other.transform);
+                Debug.Log("Unregistered: " + other.name);
+            }
+            
+        }
+    }
+
+    // Метод для удаления эмиттера из списка по атрибуту
+    public void RemoveEmiter(Transform emiterToRemove)
+    {
+        if (emiters.Contains(emiterToRemove))
+        {
+            emiters.Remove(emiterToRemove);
+            Debug.Log("Emiter removed: " + emiterToRemove.name);
+        }
+    }
+
+    void OnDisable()
+    {
+        if (addingEmiter != null)
+        {
+            StopCoroutine(addingEmiter);
+        }
+        emiters.Clear();
+        emitersToAdd.Clear();
+        
+    }
+    
+    private IEnumerator AddEmiterAfterDelay(Transform emiter)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        // Проверяем, существует ли переданный эмиттер ещё в списке emitersToAdd
+        if (emitersToAdd.Contains(emiter))
+        {
+            // Если существует, проверяем, есть ли он уже в списке emiters
+            if (!emiters.Contains(emiter))
+            {
+                // Если не существует, добавляем его в список emiters
+                emiters.Add(emiter);
+                Debug.Log("Registered: " + emiter.name);
+            }
+            emitersToAdd.Remove(emiter);
+        }
+    }
+    private IEnumerator RemoveEmiterAfterDelay(Transform emiter)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        // Проверяем, существует ли переданный эмиттер ещё в списке emitersToAdd
+        if (emitersToAdd.Contains(emiter))
+        {
+            // Если существует, проверяем, есть ли он уже в списке emiters
+            if (!emiters.Contains(emiter))
+            {
+                // Если не существует, добавляем его в список emiters
+                emiters.Add(emiter);
+                emitersToAdd.Remove(emiter);
+                Debug.Log("Registered: " + emiter.name);
+            }
+        }
+    }
+}
