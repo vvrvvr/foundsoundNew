ab199d8aec7af410847d9b9d47b167baef5dc432
v.svistunov <v.svistunov@corp.nekki.ru>
2024-07-21 20:44:23 +0300

    аутлайн у записей wip

--- foundsound_new/Assets/Scripts/Recorder/Record.cs
+++ foundsound_new/Assets/Scripts/Recorder/Record.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Record : MonoBehaviour, IInteractable
@@ -7,13 +8,22 @@ public class Record : MonoBehaviour, IInteractable
    public string Description = "Some decscription";
    public AudioClip recordAudio;
    public bool isDestroyable = false;
+   public Outline outline;
 
+   public void See()
+   {
+      Debug.Log("вижу: "+ gameObject.name);
+      outline.enabled = true;
+   }
+   
    private void Update() //удалить
    {
       if (Input.GetKeyDown(KeyCode.P))
       {
          NoteManager.Instance.AddNote(RecordName, Description, recordAudio);
       }
+
+      outline.enabled = false;
    }
    
    public void Interact()
@@ -22,11 +32,9 @@ public class Record : MonoBehaviour, IInteractable
       Debug.Log("кликнул на: "+ gameObject.name);
    }
 
-   public void See()
-   {
-      Debug.Log("вижу: "+ gameObject.name);
-   }
-   
+  
+
+  
    public void TakeRecord()
    {
       NoteManager.Instance.AddNote(RecordName, Description, recordAudio);
