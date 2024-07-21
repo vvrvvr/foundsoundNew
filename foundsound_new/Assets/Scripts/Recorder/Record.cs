173a3e0dfab151521400bd9a37ed4109c3dfba2c
v.svistunov <v.svistunov@corp.nekki.ru>
2024-07-21 22:10:18 +0300

    подбор звуков

--- foundsound_new/Assets/Scripts/Recorder/Record.cs
+++ foundsound_new/Assets/Scripts/Recorder/Record.cs
@@ -9,11 +9,23 @@ public class Record : MonoBehaviour, IInteractable
    public AudioClip recordAudio;
    public bool isDestroyable = false;
    public Outline outline;
-   public bool shouldOutlineBeEnabled;
+   private bool shouldOutlineBeEnabled;
    public Transform emitterTransform;
-   
+   private AudioSource audioSource;
+   private bool isActive = true;
+
+   private void Start()
+   {
+      audioSource = GetComponent<AudioSource>();
+      audioSource.clip = recordAudio;
+      audioSource.Play();
+   }
+
    public void See()
    {
+      if (!isActive)
+         return;
+      
       Debug.Log("вижу: "+ gameObject.name);
       if (outline != null)
          shouldOutlineBeEnabled = true;
@@ -21,6 +33,9 @@ public class Record : MonoBehaviour, IInteractable
    
    private void Update()
    {
+      if (!isActive)
+         return;
+      
       if (shouldOutlineBeEnabled)
          outline.enabled = true;
       else
@@ -32,6 +47,9 @@ public class Record : MonoBehaviour, IInteractable
    
    public void Interact()
    {
+      if (!isActive)
+         return;
+      
       TakeRecord();
       Debug.Log("кликнул на: "+ gameObject.name);
      
@@ -40,10 +58,22 @@ public class Record : MonoBehaviour, IInteractable
    public void TakeRecord()
    {
       NoteManager.Instance.AddNote(RecordName, Description, recordAudio);
-      //удалить эмиттер звука
-      // выключить звук и интерактивность
-      SignalController signalController = FindObjectOfType<SignalController>();
-      signalController.RemoveEmiter(emitterTransform);
-      Destroy(gameObject);
+      
+      if (isDestroyable) //для выбрасываемого звука
+      {
+         SignalController signalController = FindObjectOfType<SignalController>();
+         signalController.RemoveEmiter(emitterTransform);
+         Destroy(gameObject);
+      }
+      else //для звучащих предметов на уровне
+      {
+         outline.enabled = false;
+         SignalController signalController = FindObjectOfType<SignalController>();
+         signalController.RemoveEmiter(emitterTransform);
+         emitterTransform.gameObject.SetActive(false);
+         audioSource.Stop();
+         isActive = false;
+      }
+      
    }
 }
